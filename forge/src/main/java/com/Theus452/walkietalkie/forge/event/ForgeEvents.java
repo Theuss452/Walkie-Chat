c6a95f2385da5b0be8b3b5a7ea6cfc9cf46af408
Matheus <matheushsaz05@gmail.com>
2025-07-10 02:59:06 -0300

    Walkie-Chat Fabric and Forge 1.20.1

---
+++ forge/src/main/java/com/Theus452/walkietalkie/forge/event/ForgeEvents.java
@@ -0,0 +1,77 @@
+package com.Theus452.walkietalkie.forge.event;
+
+import com.Theus452.walkietalkie.forge.commands.ForgeCommands;
+import com.Theus452.walkietalkie.item.WalkieTalkieItem;
+import com.Theus452.walkietalkie.platform.Platform;
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.ChatFormatting;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeEvents {
+
+    @SubscribeEvent
+    public void onCommandsRegister(RegisterCommandsEvent event) {
+        CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
+        ForgeCommands.register(dispatcher);
+    }
+
+    @SubscribeEvent
+    public void onServerChat(ServerChatEvent event) {
+        ServerPlayer sender = event.getPlayer();
+        ItemStack mainHand = sender.getMainHandItem();
+
+        
+        event.setCanceled(true);
+
+        if (mainHand.getItem() instanceof WalkieTalkieItem) {
+            
+            String frequency = WalkieTalkieItem.getFrequency(mainHand);
+            if (frequency.isEmpty()) {
+                sender.sendSystemMessage(Component.translatable("message.walkietalkie.define.frequency"));
+                return;
+            }
+
+            
+            Component walkieTalkieMessage = Component.literal("§a[Walkie-Talkie] §f<" + sender.getDisplayName().getString() + ">§f " + event.getRawText());
+
+            
+            sender.sendSystemMessage(walkieTalkieMessage);
+
+            
+            for (ServerPlayer receiver : sender.getServer().getPlayerList().getPlayers()) {
+                if (receiver == sender) continue;
+                for (ItemStack inventoryStack : receiver.getInventory().items) {
+                    if (inventoryStack.getItem() instanceof WalkieTalkieItem) {
+                        if (frequency.equals(WalkieTalkieItem.getFrequency(inventoryStack))) {
+                            receiver.sendSystemMessage(walkieTalkieMessage);
+                            break;
+                        }
+                    }
+                }
+            }
+        } else {
+            
+            Component formattedMessage = Component.translatable("chat.type.text", sender.getDisplayName(), Component.literal(event.getRawText()));
+            double currentChatRange = Platform.HELPER.getChatRange();
+            int recipientsFound = 0;
+
+            for (ServerPlayer recipient : sender.getServer().getPlayerList().getPlayers()) {
+                if (sender.distanceToSqr(recipient) <= currentChatRange * currentChatRange) {
+                    recipient.sendSystemMessage(formattedMessage);
+                    recipientsFound++;
+                }
+            }
+
+            if (recipientsFound <= 1 && sender.getServer().getPlayerList().getPlayerCount() > 1) {
+                sender.sendSystemMessage(Component.translatable("message.walkietalkie.no_one_nearby")
+                        .withStyle(ChatFormatting.GRAY, ChatFormatting.ITALIC));
+            }
+        }
+    }
+}
